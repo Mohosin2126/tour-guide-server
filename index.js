0cfdcf6aff782393b2a9b7fb774fda395f96e1b3
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-26 18:13:32 +0800

    added index.js file env file gitignore file

---
+++ index.js
@@ -0,0 +1,34 @@
+const express= require('express')
+const app=express()
+const cors=require('cors')
+const port=process.env.PORT||5000
+require('dotenv').config()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send('Tour is running')
+})
+
+app.listen(port,()=>{
+    console.log(`Tour is running in port ${port}`)
+})
